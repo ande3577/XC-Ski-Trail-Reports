8a0c8849f4a737a1d9e05dc7e1a3c87e5a3fcd49
David S Anderson <ande3577@gmail.com>
2012-01-30 19:20:08 -0600

    Initial commit into git

---
+++ org.dsanderson.xctrailreport/src/org/dsanderson/xctrailreportActivity.java
@@ -0,0 +1,14 @@
+package org.dsanderson;
+
+import org.dsanderson.xctrailreport.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class xctrailreportActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
