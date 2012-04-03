b00e63221a701c52d2a50e424f02ca4c76651f39
David S Anderson <ande3577@gmail.com>
2012-04-02 23:22:41 -0500

    cleaned up last page scanning

--- org.dsanderson.morctrailreport.parser/src/org/dsanderson/morctrailreport/parser/MorcAllReportScanner.java
+++ org.dsanderson.morctrailreport.parser/src/org/dsanderson/morctrailreport/parser/MorcAllReportScanner.java
@@ -52,17 +52,7 @@ public class MorcAllReportScanner {
 
 	public int findLastPage() {
 		if (findNext("\\Q<span class=\"first_last\">\\E")) {
-			String lastPageString = scanner.nextLine();
-			if (lastPageString == null || lastPageString.isEmpty())
-				return 1;
-			String split[] = lastPageString.split("[\"\\&]");
-			if (split.length <= 3)
-				return 1;
-			split = split[1].split("\\Qpage\\E");
-			if (split.length < 2)
-				return 1;
-			lastPageString = split[1];
-			
+			String lastPageString = scan("\\Qpage\\E", "[\"\\&]", ".*");
 			if (lastPageString != null && !lastPageString.isEmpty()) {
 				return Integer.parseInt(lastPageString);
 			}
